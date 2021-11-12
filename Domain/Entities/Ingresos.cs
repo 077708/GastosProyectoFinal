dee7e88527bdee6b4574fe2995fccbcab1275747
Paulo Mayorga <kuroko0010@gmail.com>
2021-11-12 17:26:05 -0600

    Add

--- Domain/Entities/Ingresos.cs
+++ Domain/Entities/Ingresos.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,16 @@ namespace Domain.Entities
 {
     public class Ingresos
     {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Ingreso { get; set; }
+
+        public CategoriaIngresos CategoryExpense { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public byte[] Image { get; set; }
     }
 }
